2b427c2225af58e11a74b00f7f897faa77935040
user <email@mail.ru>
2023-09-18 21:01:50 +0300

    Create Project

---
+++ Lession One/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lession_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Найти алгебраическую сумму для выражения: 1k + 2k + 3k + … +Nk.N и степень k вводит пользователь.
+             * Пример работы программы:
+             * N = 5, k = 2, Сумма : 55
+            */
+        }
+    }
+}
